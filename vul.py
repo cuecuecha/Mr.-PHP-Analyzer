81446c493077cb647c079f6092118d4036770f9b
cuecuecha <hdzcuecuecha@gmail.com>
2017-10-19 21:18:09 -0700

    actualizacion xss, lfi y rfi

--- vul.py
+++ vul.py
@@ -8,17 +8,18 @@ class get_vul:
 def get_xss():
 	list =[]
 	table=[]
-	m= re.compile(r"(?<!(include\s))(\$\_(POST|REQUEST|GET)\[.*\])",re.I) #LFI
 	n= re.compile(r"(?<!(trim\())(\$\_(POST|REQUEST|GET)\[.*\])",re.I)
 	o = re.compile(r"(?<!(htmlentities\())(\$\_(POST|REQUEST|GET)\[.*\])",re.I)
-	p = re.compile(r"(?<!(strip\_tags\())(\$\_(POST|REQUEST|GET)\[.*\])",re.I)
+	p = re.compile(r"(?<!(strip\_tags\())(\$\_(POST|REQUEST|GET)\[.*\])",re.I) 
 	q = re.compile(r">.*?<\s*\/?[\w\s]+>",re.I)
-	lexpresiones = [n,o,p,q]
+	r = re.compile(r"[\"\'].*?>",re.I) #HTML breaking
+	s = re.compile(r"<base\b.+?\bhref\b.+?>",re.I) # Common JavaScript injection points (forms)
+	t = re.compile(r"\bon\w+\s*=",re.I) #HTML event handler
+	lexpresiones = [t]
 	#file=["php/w3af-moth/webroot/moth/w3af/core/wml_parser/form_sql_injection.php"]
 	file=files.get_listfiles()
 	for fs in file:
 		with open(fs,"r") as f:
-			print(fs)
 			for line in f.readlines():
 				line = line.strip()
 				if re.match(r'^\s*$', line): continue #If line is empty, continue with the next one
@@ -27,6 +28,7 @@ def get_xss():
 			#if re.match(r'([^(htmlespecialchars)]|([^(strip_tags)])|([^(trim)])|([^(htmlentities)]))(\$\_(GET|POST|REQUEST)\[(\"|\')[aA-zZ]+(\"|\')])',line):
 				for i in range(0,len(lexpresiones)):
 					if lexpresiones[i].search(line):						
+						print(fs)
 						print(line)
 
 def get_lfi_rfi():
@@ -43,7 +45,29 @@ def get_lfi_rfi():
 	v = re.compile(r"\breadfile\b.*?\(.+?\)",re.I) #Critical PHP function readfile
 	y = re.compile(r"\binclude(_once)?\b.*?;",re.I) #Critical PHP function include
 	x = re.compile(r"\brequire(_once)?\b.*?;",re.I) #Critical PHP function require
-	lexpresiones = [m,n,o,p,q,r,s,u,v,y,x]
+	#RFI
+	a = re.compile(r"\(\)\s*\{.*?;\s*\}\s*;",re.I) #Shellshock (CVE-2014-6271)
+	b = re.compile(r"(\b(do|while|for)\b.*?\([^)]*\).*?\{)|(\}.*?\b(do|while|for)\b.*?\([^)]*\))",re.I) #C-style loops
+	c = re.compile(r"[=(].+?\?.+?\:",re.I) #C-style ternary operator
+	d = re.compile(r"\\u00[a-f0-9]{2}",re.I) #Octal entity
+	e = re.compile(r"\\x0*[a-f0-9]{2}",re.I) # Hex entity
+	f = re.compile(r"\\\d{2,3}",re.I) #Unicode entity
+	g = re.compile(r"\/\/input",re.I) #PHP input stream
+	h = re.compile(r" <\?(?!xml\s)",re.I) #PHP opening tag
+	i = re.compile(r"\bcall_user_func\b.*?\(.+?\)",re.I) #Critical PHP function call_user_func
+	j = re.compile(r"\bcreate_function\b.*?\(.+?\)",re.I) 
+	k = re.compile(r" \beval\b.*?(\(.+?\)|\{.+?\})",re.I) #Critical function eval
+	l = re.compile(r"\bexec\b.*?\(.+?\)",re.I) #Critical function exec
+	axa = re.compile(r"\bmove_uploaded_file\b.*?\(.+?\)",re.I)
+	axb = re.compile(r"\bpassthru\b.*?\(.+?\)",re.I) #Critical PHP function 'passthru'
+	axc = re.compile(r"\bp(roc_)?open\b.*?\(.+?\)",re.I) #Critical PHP function 'popen/proc_open'
+	axd = re.compile(r"shell_exec.*?\(.+?\)",re.I) #critical PHP function 'shell_Exec'
+	axe = re.compile(r"system.*?\(.+?\)") # Critical PHP function 'system'
+	axf = re.compile(r"\bpreg_(replace|match)\b.*?\(.+?\)") #Critical PHP function 'preg_match/preg_replace'
+	axg = re.compile(r"\{\s*\$\s*\{.+?\}\s*\}",re.I) #PHP complex curly syntax
+	axh = re.compile(r"\bexec\b.+?\bxp_cmdshell\b",re.I)#MSSQL code execution 'xp_cmdshell'
+	axi = re.compile(r"")
+	lexpresiones = [axh]
 	#file=["php/w3af-moth/webroot/moth/w3af/core/wml_parser/form_sql_injection.php"]
 	file=files.get_listfiles()
 	for fs in file:
