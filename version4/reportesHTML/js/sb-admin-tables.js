f0a6b7472cef6eef152da1599be4e22bd3ed774d
root <root@localhost.localdomain>
2017-11-10 13:17:36 -0600

    reporte 90% listo

---
+++ version4/reportesHTML/js/sb-admin-tables.js
@@ -0,0 +1,37 @@
+
+			$(document).ready(function() {
+			  $('#dataTable1').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable3').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable1').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable3').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable1').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable3').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable9').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable1').DataTable();
+			});
+		
+			$(document).ready(function() {
+			  $('#dataTable3').DataTable();
+			});
+		
