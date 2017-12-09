797053d121e64044b86382260fe725e9d58fa879
root <root@localhost.localdomain>
2017-12-08 18:27:44 -0600

    update project

--- puzzle/generateHtml.py
+++ puzzle/generateHtml.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from webbrowser import open_new_tab
+import datetime
 import time
+import os.path
+from random import randint
 # -*- coding: utf-8 -*-
 __author__="Coronado Gozain Saine, Hernandez Cuecuecha Jorge Alberto"
 __copyright__="Copyright 2017, UNAM-CERT"
@@ -10,81 +13,81 @@ __version__="1.0"
 __status__="Prototype"
 class get_html:
 	pass
-ahora = time.strftime("%Y%m%d-%H")
+times = time.strftime("%Y%m%d-%H")
 
 #funcion para crear reporte
+
 def creareporte():
-	ahora = time.strftime("%Y%m%d-%H")
-	nombreArchivo = ahora + '.html'
+	ahora = datetime.datetime.now().strftime("%a%Y%m%d-%H%M")
+	nombreArchivo = ahora+'.html'
 	reporte = open('/opt/mrphpanalyzer/reportesHTML/'+nombreArchivo,'a')
 	return reporte
 
+
+
 #funcion donde se crea sólo el header del reporte
 def headerhtml(reporte):
-	ahora = time.strftime("%Y%m%d-%H")
-	nombreArchivo = ahora + '.html'
-
 	reporte.write ("""
-		<!DOCTYPE html>
-		<html lang="en">
+			<!DOCTYPE html>
+			<html lang="en">
 
-		<head>
-		  <meta charset="utf-8">
-		  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-		  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		  <meta name="description" content="">
-		  <meta name="author" content="">
-		  <title>Mr. PHP Analyzer</title>
-		  <!-- Bootstrap core CSS-->
-		  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		  <!-- Custom fonts for this template-->
-		  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		  <!-- Page level plugin CSS-->
-		  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-		  <!-- Custom styles for this template-->
-		  <link href="css/sb-admin.css" rel="stylesheet">
-		  <link href="css/style.css" rel="stylesheet">
-		</head>
+			<head>
+			  <meta charset="utf-8">
+			  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+			  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			  <meta name="description" content="">
+			  <meta name="author" content="">
+			  <title>Mr. PHP Analyzer</title>
+			  <!-- Bootstrap core CSS-->
+			  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+			  <!-- Custom fonts for this template-->
+			  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+			  <!-- Page level plugin CSS-->
+			  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+			  <!-- Custom styles for this template-->
+			  <link href="css/sb-admin.css" rel="stylesheet">
+			  <link href="css/style.css" rel="stylesheet">
+			</head>
 
-		<body class="fixed-nav sticky-footer bg-dark" id="page-top">
-		  <!-- Navigation-->
-		  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-		    <a class="navbar-brand" href="index.html">Mr. PHP Analyzer</a>
-		    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-		      <span class="navbar-toggler-icon"></span>
-		    </button>
-		    <div class="collapse navbar-collapse" id="navbarResponsive">
-		      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-		        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Current Report">
-		          <a class="nav-link" href="#">
-		            <i class="fa fa-fw fa-dashboard"></i>
-		            <span class="nav-link-text">Current Report</span>
-		          </a>
-		        </li>
-		        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="History">
-		          <a class="nav-link" href="history.html">
-		            <i class="fa fa-fw fa-area-chart"></i>
-		            <span class="nav-link-text">History</span>
-		          </a>
-		        </li>
-		      </ul>
-		      <ul class="navbar-nav sidenav-toggler">
-		        <li class="nav-item">
-		          <a class="nav-link text-center" id="sidenavToggler">
-		            <i class="fa fa-fw fa-angle-left"></i>
-		          </a>
-		        </li>
-		      </ul>
+			<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+			  <!-- Navigation-->
+			  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+			    <a class="navbar-brand" href="index.html">Mr. PHP Analyzer</a>
+			    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			      <span class="navbar-toggler-icon"></span>
+			    </button>
+			    <div class="collapse navbar-collapse" id="navbarResponsive">
+			      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+			        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Current Report">
+			          <a class="nav-link" href="#">
+			            <i class="fa fa-fw fa-dashboard"></i>
+			            <span class="nav-link-text">Current Report</span>
+			          </a>
+			        </li>
+			        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="History">
+			          <a class="nav-link" href="history.html">
+			            <i class="fa fa-fw fa-area-chart"></i>
+			            <span class="nav-link-text">History</span>
+			          </a>
+			        </li>
+			      </ul>
+			      <ul class="navbar-nav sidenav-toggler">
+			        <li class="nav-item">
+			          <a class="nav-link text-center" id="sidenavToggler">
+			            <i class="fa fa-fw fa-angle-left"></i>
+			          </a>
+			        </li>
+			      </ul>
 
-		    </div>
-		  </nav>
-		  <div class="content-wrapper">
-		    <div class="container-fluid">
-		      <!-- Breadcrumbs-->
-		      <div class="breadcrumb" style="height: 350px;">
-		        <img class="img-responsive" src="img/cert.png" alt="UNAM-CERT" style="display: block; margin:auto; width: 25%;">
-		      </div>
-		   """)
+			    </div>
+			  </nav>
+			  <div class="content-wrapper">
+			    <div class="container-fluid">
+			      <!-- Breadcrumbs-->
+			      <div class="breadcrumb" style="height: 350px;">
+			        <img class="img-responsive" src="img/cert.png" alt="UNAM-CERT" style="display: block; margin:auto; width: 25%;">
+			      </div>
+			   """)
 
 #funcion donde sea crea el canvas de las gráficas
 def graphic(reporte):
@@ -142,9 +145,9 @@ def jsgraphic(vulnes):
 		    labels: ["SQLi", "XSS", "Session Cookie", "Send S. Info", "LFI&RFI", "P. Traversal", "I. Comandos","I. Código"],
 		    datasets: [{
 		      label: "Revenue",
-		      backgroundColor: "rgba(2,117,216,1)",
+		      backgroundColor: ['#F58C46', '#FBF302', '#3D8CFA', '#FBAD54', '#F5BE82', '#26C328', '#FD0000', '#FB5138'],
 		      borderColor: "rgba(2,117,216,1)",
-		      data: [%s, %s, %s, %s, %s, %s, %s, %s],
+		      data: [%s, %s, %s, %s, %s, %s, %s, 0],
 		    }],
 		  },
 		  options: {
@@ -183,15 +186,15 @@ def jsgraphic(vulnes):
 		  data: {
 		    labels: ["SQLi", "XSS", "Session Cookie", "Send S. Info", "LFI&RFI", "P. Traversal", "I. Comandos","I. Código"],
 		    datasets: [{
-		      data: [%s, %s,%s, %s,%s,%s,%s,%s],
-		      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745', '#007bff', '#dc3545', '#ffc107', '#28a745'],
+		      data: [%s, %s,%s, %s,%s,%s,%s,0],
+		      backgroundColor: ['#F58C46', '#FBF302', '#3D8CFA', '#FBAD54', '#F5BE82', '#26C328', '#FD0000', '#FB5138'],
 		    }],
 		  },
 		});
 	"""%(str(vulnes.get('1')),str(vulnes.get('2')), str(vulnes.get('3')),str(vulnes.get('4')),str(vulnes.get('5')),
-		str(vulnes.get('6')),str(vulnes.get('7')),str(vulnes.get('8')),
+		str(vulnes.get('6')),str(vulnes.get('7')),
 		str(vulnes.get('1')),str(vulnes.get('2')), str(vulnes.get('3')),str(vulnes.get('4')),str(vulnes.get('5')),
-		str(vulnes.get('6')),str(vulnes.get('7')),str(vulnes.get('8'))
+		str(vulnes.get('6')),str(vulnes.get('7'))
 		) )
 
 #funcion donde imprime el total de cada vulnerabilidad
@@ -240,7 +243,7 @@ def tablefhtml(table,reporte, option):
 
 	
 	#js = estadisticas.format()
-	reportes = tob.format(option,table,ahora)
+	reportes = tob.format(option,table,times)
 	reporte.write(reportes)
 
 
