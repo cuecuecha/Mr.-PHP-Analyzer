5c2e7f334c175d7ba86e0dc4650dc2cc28b2d113
root <root@localhost.localdomain>
2017-12-14 13:18:08 -0600

    update history

--- puzzle/his.py
+++ puzzle/his.py
@@ -12,65 +12,70 @@ reporte = open('/opt/mrphpanalyzer/reportesHTML/history.html','w')
 a = glob.glob("/opt/mrphpanalyzer/reportesHTML/*.html")
 
 reporte.write ("""
-	<!DOCTYPE html>
-	<html lang="en">
+			<!DOCTYPE html>
+			<html lang="en">
 
-	<head>
-	  <meta charset="utf-8">
-	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	  <meta name="description" content="">
-	  <meta name="author" content="">
-	  <title>Mr. PHP Analyzer</title>
-	  <!-- Bootstrap core CSS-->
-	  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	  <!-- Custom fonts for this template-->
-	  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	  <!-- Page level plugin CSS-->
-	  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-	  <!-- Custom styles for this template-->
-	  <link href="css/sb-admin.css" rel="stylesheet">
-	  <link href="css/style.css" rel="stylesheet">
-	</head>
+			<head>
+			  <meta charset="utf-8">
+			  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+			  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			  <meta name="description" content="">
+			  <meta name="author" content="">
+			  <title>Mr. PHP Analyzer</title>
+			  <!-- Bootstrap core CSS-->
+			  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+			  <!-- Custom fonts for this template-->
+			  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+			  <!-- Page level plugin CSS-->
+			  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+			  <!-- Custom styles for this template-->
+			  <link href="css/sb-admin.css" rel="stylesheet">
+			  <link href="css/style.css" rel="stylesheet">
+  			  <link rel="shortcut icon" type="text/css" href="img/pirate.ico">
 
-	<body class="fixed-nav sticky-footer bg-dark" id="page-top">
-	  <!-- Navigation-->
-	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-	    <a class="navbar-brand" href="index.html">Mr. PHP Analyzer</a>
-	    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-	      <span class="navbar-toggler-icon"></span>
-	    </button>
-	    <div class="collapse navbar-collapse" id="navbarResponsive">
-	      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-	        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="History">
-	          <a class="nav-link" href="history.html">
-	            <i class="fa fa-fw fa-area-chart"></i>
-	            <span class="nav-link-text">History</span>
-	          </a>
-	        </li>
-	      </ul>
-	      <ul class="navbar-nav sidenav-toggler">
-	        <li class="nav-item">
-	          <a class="nav-link text-center" id="sidenavToggler">
-	            <i class="fa fa-fw fa-angle-left"></i>
-	          </a>
-	        </li>
-	      </ul>
+			</head>
 
-	    </div>
-	  </nav>
-	  <div class="content-wrapper">
-	    <div class="container-fluid">
-	      <!-- Breadcrumbs-->
-	      <div class="breadcrumb" style="height: 350px;">
-	        <img class="img-responsive" src="img/cert.png" alt="UNAM-CERT" style="display: block; margin:auto; width: 25%;">
-	      </div>
+			<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+			  <!-- Navigation-->
+			  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+			  <img src="img/pirate.png" style="width: 4%;"> 
+			    <a class="navbar-brand" href="index.html">Mr. PHP Analyzer</a>
+			    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			      <span class="navbar-toggler-icon"></span>
+			    </button>
+			    <div class="collapse navbar-collapse" id="navbarResponsive">
+			      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion" style="top: 16px;">
+			        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="History">
+			          <a class="nav-link" href="history.html">
+			            <i class="fa fa-fw fa-area-chart"></i>
+			            <span class="nav-link-text">History</span>
+			          </a>
+			        </li>
+			      </ul>
+			      <ul class="navbar-nav sidenav-toggler">
+			        <li class="nav-item">
+			          <a class="nav-link text-center" id="sidenavToggler">
+			            <i class="fa fa-fw fa-angle-left"></i>
+			          </a>
+			        </li>
+			      </ul>
+
+			    </div>
+			  </nav>
+			  <div class="content-wrapper">
+			    <div class="container-fluid">
+			      <!-- Breadcrumbs-->
+			      <div class="breadcrumb" style="height: auto;">
+			        <img class="img-responsive" src="img/cert.png" alt="UNAM-CERT" style="display: block; margin:auto; width: 25%;">
+			      </div>
+
 	   """)
 b = " "
 if a:
 	for elem in a:
-		ap = '<a href="'+str(elem)+'">'+str(elem)+'</a> <br>'
-		b += str(ap)
+		if not "history.html" in elem:
+			ap = '<a href="'+str(elem)+'">'+str(elem)+'</a> <br>'
+			b += str(ap)
 	reporte.write("""
 		%s
 		<footer class="sticky-footer">
