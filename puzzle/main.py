a57ec8c3a8b926248ec323ae90d64d3e7aa26cf6
root <root@localhost.localdomain>
2018-01-08 11:54:12 -0600

    funcionando reporte

--- puzzle/main.py
+++ puzzle/main.py
@@ -18,65 +18,66 @@ parser.add_argument('-r' , nargs=1,  help="path")
 parser.add_argument('-v' , nargs=1, help="vulnerability")
 parser.add_argument('--sos', action="store_true", help="actions list")
 option = parser.parse_args()
-
+a=option.r[0].rsplit("/")
+pag=a[-2]
 if option.r and option.v: #if option.r and option.v have some value, it will run functions depending of the options 
 	files.get_files(option.r[0])
 	v=version.get_version()
 	if option.v[0] is "1": #for the opction sqli, only this vulnerability
-		generateHtml.headerhtml(generateHtml.creareporte())
+		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 		report.create_reportlist()
 		report.create_re_vulist("1")
 		csvcreate.create_report("11")
 		generateHtml.footer(generateHtml.creareporte())	
 	elif option.v[0] is "2": #for the opction xss, only this vulnerability
-		generateHtml.headerhtml(generateHtml.creareporte())
+		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 		report.create_reportlist()
 		report.create_re_vulist("2")
 		csvcreate.create_report("11")	
 		generateHtml.footer(generateHtml.creareporte())		
 	elif option.v[0] is "3": #for the opction session's cookies, only this vulnerability
-		generateHtml.headerhtml(generateHtml.creareporte())
+		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 		report.create_reportlist()
 	        report.create_re_vulist("3")
 	        csvcreate.create_report("11")
 		generateHtml.footer(generateHtml.creareporte())
 	elif option.v[0] is "4": #for the opction send sensible information, only this vulnerability
-		generateHtml.headerhtml(generateHtml.creareporte())
+		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 		report.create_reportlist()
 		report.create_re_vulist("4")
 		csvcreate.create_report("11")
 		generateHtml.footer(generateHtml.creareporte())
 	elif option.v[0] is "5": #for the opction LFI & RFI, only this vulnerability
-		generateHtml.headerhtml(generateHtml.creareporte())
+		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 		report.create_reportlist()
                 report.create_re_vulist("5")
                 csvcreate.create_report("11")
                 generateHtml.footer(generateHtml.creareporte())
 	elif option.v[0] is "6": #for the opction path traversal, only this vulnerability
-		generateHtml.headerhtml(generateHtml.creareporte())
+		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 		report.create_reportlist()
                 report.create_re_vulist("6")
                 csvcreate.create_report("11")
                 generateHtml.footer(generateHtml.creareporte())
 	elif option.v[0] is "7": #for the opction command injection, only this vulnerability
-		generateHtml.headerhtml(generateHtml.creareporte())
+		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 		report.create_reportlist()
                 report.create_re_vulist("7")
                 csvcreate.create_report("11")
                 generateHtml.footer(generateHtml.creareporte())
-                
+
 	elif option.v[0] is "9": #for the opction obsolete functions only these
 		if v is "7":
 			print"Didn't find obsolete functions"    	
 		else:
-        		generateHtml.headerhtml(generateHtml.creareporte())
+        		generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 			report.create_reportlist()
         		report.create_re_funclist()
         		csvcreate.create_report("12")
         		generateHtml.footer(generateHtml.creareporte())	
 
 elif option.r: #if option.r has some value but option.v does not have, it will run functions of all vulnerabilities  
-	generateHtml.headerhtml(generateHtml.creareporte())
+	generateHtml.headerhtml(generateHtml.creareporte(), a[-2])
 	generateHtml.graphic(generateHtml.creareporte())
 	prov=["1","2","3","4","5","6","7","8"]
 	files.get_files(option.r[0])
@@ -113,3 +114,7 @@ if option.sos:
 
 os.system('python /opt/mrphpanalyzer/puzzle/his.py')
 generateHtml.listfiles()
+
+
+def nameProject():
+	print a[-2]
