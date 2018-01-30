c64e552148c5d8cd1696d55c9770fb7e64bf0ea6
root <root@localhost.localdomain>
2018-01-29 19:24:05 -0600

    figura

--- uninstall.sh
+++ uninstall.sh
@@ -6,9 +6,15 @@ if [[ $EUID -ne 0 ]]; then
 	echo -e "\n \033[0;31mDebe ejecutarse como root \033[0m\n" 1>&2
 	exit 1
 fi
+	echo "
+░░░███████ ]▄▄▄▄▄▄▄▄         'La autocrítica
+▂▄▅█████████▅▄▃▂           ☻/   está muy bien,
+███████████████████].     /▌     mientras no tenga
+ ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤..        /\      que ver con uno mismo﻿' "
+	echo ""
 	echo "Deleting file /opt/mrphpanalyzer" 
 		rm -fr /opt/mrphpanalyzer
-	echo "Uninstall"
+	echo "Uninstalling from your system"
 		rm -f /bin/mrphpanalyzer
 	echo "Finished uninstall"
 
